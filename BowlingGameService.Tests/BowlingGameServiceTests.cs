17f3dc65ab1e3a7b5920e950b527dd65fa1d5880
Dennis Wong <cfwwong@gmail.com>
2022-09-13 22:01:06 +0100

    Initial commit for Bowling Game Kata

---
+++ BowlingGameService.Tests/BowlingGameServiceTests.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Net.NetworkInformation;
+using FluentAssertions;
+namespace BowlingGameService.Tests;
+
+public class Tests
+{
+    private BowlingScoreCalculator _bowlingScoreCalculator;
+
+    [SetUp]
+    public void Setup()
+    {
+        _bowlingScoreCalculator = new BowlingScoreCalculator();
+    }
+
+    [TestCase(new int[] { 4 }, 4)] //signle roll
+    [TestCase(new int[] { 4, 3 }, 7)] //no spare nor strike in first frame
+    [TestCase(new int[] { 4, 6, 5, 3 }, 23)] //spare in first frame
+    [TestCase(new int[] { 4, 6, 5, 5, 3, 2 }, 33)] //first 2 consective spares
+    [TestCase(new int[] { 4, 6, 5, 5, 3, 7, 9, 0 }, 56)] //spare in first 3 frames but not the 4th frame
+    [TestCase(new int[] { 4, 6, 5, 3, 3, 7, 9, 0 }, 51)] //spare in 1st & 3rd frames
+    [TestCase(new int[] { 4, 6, 5, 3, 3, 7, 9, 0, 3, 7, 2, 7, 3, 4, 6, 2, 3, 7, 9, 1, 5 }, 121)] //spare in 3rd, 5th & 10th frames
+    [TestCase(new int[] { 10, 6, 3 }, 28)] //strike in first frame
+    [TestCase(new int[] { 10, 10, 8, 0 }, 54)] //strike in 1st & 2nd frames but not the 3td frame
+    [TestCase(new int[] { 10, 8, 1, 10, 0, 7 }, 52)] //strike in 1st & 3rd frames
+    [TestCase(new int[] { 10, 4, 3, 8, 0, 10, 3, 4, 9, 0, 10, 5, 2, 7, 2, 1, 4 }, 103)] //played 10 frames, strike in 1st, 4th & 7th frames
+    [TestCase(new int[] { 10, 4, 3, 8, 0, 10, 3, 4, 9, 0, 10, 5, 2, 7, 2, 10, 3, 5 }, 116)] //played 10 frames, strike in 1st, 4th & 10th frames
+    [TestCase(new int[] { 10, 4, 6, 8, 0, 10, 3, 4, 9, 1, 10, 5, 2, 7, 2, 10, 3, 5 }, 141)] //played 10 frames, strike in 1st, 4th & 10th frames, spare in 2nd & 6th frames
+    [TestCase(new int[] { 10, 4, 6, 8, 0, 10, 3, 4, 9, 1, 10, 5, 2, 7, 2, 10, 3, 7 }, 143)] //played 10 frames, strike in 1st, 4th & 10th frames, spare in 2nd, 6th & bonus frames
+    [TestCase(new int[] { 10, 4, 6, 8, 0, 10, 3, 4, 9, 1, 10, 5, 2, 7, 2, 10, 10, 10 }, 153)] //played 10 frames, spare in 2nd, 6th & bonus frames, strike in 1st, 4th, 10th & bonus frames
+    public void Given_Number_Of_Pin_Knocks_Down_Return_Score(int[] numPinKnockedDown, int expectedScore)
+    {
+        _bowlingScoreCalculator.CalculateScore(numPinKnockedDown).Should().Be(expectedScore);        
+    }
+}
---
+++ BowlingGameService/BowlingGameService.cs
@@ -0,0 +1,34 @@
+﻿namespace BowlingGameService;
+
+public class BowlingScoreCalculator
+{
+    public int CalculateScore(int[] numPinKnockedDown)
+    {
+        var score = 0;
+        var numRoll = 0;
+        var normalRolls = 20;
+        const int numPins = 10;
+
+        for (int i = 0; i<numPinKnockedDown.Count(); i++) 
+        {
+            numRoll++;
+
+            if (i < normalRolls) //if not bouns roll
+                score += numPinKnockedDown[i];
+
+            if ((numRoll%2 == 0) && (numPinKnockedDown[i] + numPinKnockedDown[i-1] == numPins) && (i < normalRolls)) //spare within 10 frames
+            {
+                score += numPinKnockedDown[i + 1];
+            }
+
+            if ((numRoll%2 == 1) && (numPinKnockedDown[i] == numPins) && (i < normalRolls)) //strike before the last roll
+            {
+                score = score + numPinKnockedDown[i+1] + numPinKnockedDown[i+2];
+                numRoll++;
+                normalRolls--;
+            }
+        }
+
+        return score;
+    }
+}
